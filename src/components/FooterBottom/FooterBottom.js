58dacc4e73a3288a4bc0e371e7ebe0eb15fa69b3
BIbek Neupane <61085978+BibekNeupanee@users.noreply.github.com>
2023-06-06 18:50:33 +0545

    edited footer bottom

--- src/components/FooterBottom/FooterBottom.js
+++ src/components/FooterBottom/FooterBottom.js
@@ -1,25 +1,19 @@
 import React, { useMemo } from "react";
 import "../FooterBottom/FooterBottom.scss";
 import facebook from "../../SVG/facebook.svg";
-import twitter from "../../SVG/twitter.svg";
 import instagram from "../../SVG/instagram.svg";
 
-
-
 function FooterButtom() {
-  
   return (
     <>
       <div className="social">
         <a href="https://www.facebook.com/Nepcloth">
           <img src={facebook} alt="Facebook" />
         </a>
-        <a href="">
-          <img src={twitter} alt="Twitter" />
-        </a>
         <a href="https://www.instagram.com/nepalese_clothing/">
           <img src={instagram} alt="Instagram" />
         </a>
+        https://goo.gl/maps/bLmR3erxAd6QHP3Z9
       </div>
     </>
   );
