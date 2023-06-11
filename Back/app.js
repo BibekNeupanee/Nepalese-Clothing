0d51975277a7779588c379b46b681992694242ba
BIbek Neupane <61085978+BibekNeupanee@users.noreply.github.com>
2023-06-11 11:51:15 +0545

    rest api implemented

--- Back/app.js
+++ Back/app.js
@@ -1,4 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const app = express();
+
+app.use(bodyParser.json());
+
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -6,27 +12,35 @@ const connection = mysql.createConnection({
   database: "nepalese_clothing",
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL database:", err);
-    return;
-  }
-  console.log("Connected to MySQL database!");
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM product", (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+
+    res.json(results);
+  });
 });
 
-connection.query(`SELECT * FROM  product` , (err, results) => {
-  if (err) {
-    console.error("Error executing MySQL query:", err);
-    return;
-  }
+app.post("/", async function (request, response) {
+  const { name, price } = request.body;
+  console.log(request.body);
+  try {
+    await connection.query(
+      "INSERT INTO product (name, price) VALUES (?, ?)",
+      [name, price]
+    );
+    console.log("LSKFLSKDJFLSKDFJ");
+    return response.sendStatus(201);
 
-  console.log("Retrieved data:", results);
+  } catch (e) {
+    console.log(e);
+    return response.status(500).send();
+  }
 });
 
-connection.end((err) => {
-  if (err) {
-    console.error("Error closing MySQL connection:", err);
-    return;
-  }
-  console.log("MySQL connection closed!");
+app.listen(3000, function () {
+  console.log("Server Running at port 3000");
 });
